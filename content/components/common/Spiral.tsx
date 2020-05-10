ed13eb5535c1211c5aa3c574daa5237d141360ab
Felix Roos <roos@entrecode.de>
2020-05-10 13:29:25 +0200

    spiral min / max + hide flags

--- content/components/common/Spiral.tsx
+++ content/components/common/Spiral.tsx
@@ -1,30 +1,32 @@
-import React from "react"
-import { useHover, useGesture, useDrag } from "react-use-gesture"
+import React from 'react';
+import { useHover, useGesture, useDrag } from 'react-use-gesture';
 
-export type Line = [number, number, string]
+export type Line = [number, number, string];
 export type Label = {
-  label: string
-  angle: number
-  fill: string
-  color: string
-}
+  label: string;
+  angle: number;
+  fill: string;
+  color: string;
+};
 
 export interface SpiralProps {
-  zoom?: number
-  width?: number
-  height?: number
-  spin?: number
-  min?: number
-  max?: number
-  stroke?: string
-  precision?: number
-  strokeWidth?: number
-  strokeLinecap?: "round" | "butt" | "square" | "inherit"
-  getRadius?: (angle?: number, maxRadius?: number, zoom?: number) => number
-  lines?: Line[]
-  labels?: Label[]
-  fontSize?: number
-  onTrigger?: (index: number) => void
+  zoom?: number;
+  width?: number;
+  height?: number;
+  spin?: number;
+  min?: number;
+  max?: number;
+  stroke?: string;
+  precision?: number;
+  strokeWidth?: number;
+  strokeLinecap?: 'round' | 'butt' | 'square' | 'inherit';
+  getRadius?: (angle?: number, maxRadius?: number, zoom?: number) => number;
+  lines?: Line[];
+  labels?: Label[];
+  fontSize?: number;
+  onTrigger?: (index: number) => void;
+  hideLabels?: boolean; // TBD: implement in spiral
+  hideLines?: boolean; // TBD: implement in spiral
 }
 
 export default function Spiral({
@@ -42,77 +44,59 @@ export default function Spiral({
   lines,
   labels,
   fontSize,
-  onTrigger,
+  onTrigger
 }: SpiralProps) {
-  zoom = zoom || 1
-  spin = spin || 0
-  fontSize = fontSize || 10
-  min = min || 0
-  precision = Math.abs(precision || 1)
-  strokeWidth = strokeWidth || 1
-  width = width || 600
-  height = height || 400
-  const maxPositions = 20000
-  const center = [width / 2, height / 2]
-  let maxRadius = Math.sqrt(width * width + height * height) / 2
-  let currentRadius = min * zoom * maxRadius
-  let dots = []
+  zoom = zoom || 1;
+  spin = spin || 0;
+  fontSize = fontSize || 10;
+  min = min || 0;
+  precision = Math.abs(precision || 1);
+  strokeWidth = strokeWidth || 1;
+  width = width || 600;
+  height = height || 400;
+  const maxPositions = 20000;
+  const center = [width / 2, height / 2];
+  let maxRadius = Math.sqrt(width * width + height * height) / 2;
+  let currentRadius = min * zoom * maxRadius;
+  let dots = [];
   const rad = (angle) =>
-    getRadius ? getRadius(angle, maxRadius, zoom) : angle * zoom * maxRadius
-
+    getRadius ? getRadius(angle, maxRadius, zoom) : angle * zoom * maxRadius;
   while (
     currentRadius < maxPositions &&
     (!dots.length ||
       (Math.abs(dots[0][1]) <= maxRadius &&
         (!max || Math.abs(dots[0][0]) <= max)))
   ) {
-    const angle = currentRadius / zoom / maxRadius
-    dots.unshift([angle, rad(angle)])
-    currentRadius += 1 / precision
+    const angle = currentRadius / zoom / maxRadius;
+    dots.unshift([angle, rad(angle)]);
+    currentRadius += 1 / precision;
   }
   if (max) {
-    const lastAngle = Math.min(dots[0][0], max)
-    dots[0][0] = lastAngle
-    dots[0][1] = rad(lastAngle)
+    const lastAngle = Math.min(dots[0][0], max);
+    dots[0][0] = lastAngle;
+    dots[0][1] = rad(lastAngle);
   }
   dots = dots.map(([angle, radius]) => {
-    return spiralPosition(angle, radius, spin, ...center)
-  })
+    return spiralPosition(angle, radius, spin, ...center);
+  });
   const lineProps: [[number, number], [number, number], string][] = (
     lines || []
   ).map(([a, b, color]) => {
     return [
       spiralPosition(a, rad(a), spin, ...center),
-      //spiralPosition(a, rad(a + b), spin, ...center),
       spiralPosition(b, rad(b), spin, ...center),
-      color,
-    ]
-  })
-  /*
-  onTouchStart={(e) => {
-          e.preventDefault()
-          e.stopPropagation()
-        }}
-        onTouchMove={(e) => {
-          e.preventDefault()
-          e.stopPropagation()
-        }}
-
-              {...useGesture({
-                onMoveStart: ({ down }) => {
-                  onTrigger && onTrigger(i)
-                },
-                })()}
-        */
+      color
+    ];
+  });
   return (
     <>
       <svg width={width} height={height}>
         <path
-          d={`M${dots.join("L")}`}
-          stroke={stroke || "gray"}
+          d={`M${dots.join('L')}`}
+          stroke={stroke || 'gray'}
           strokeWidth={strokeWidth}
           fill="none"
-          strokeLinecap={strokeLinecap || "round"}
+          strokeLinecap={strokeLinecap || 'round'}
         />
         {lineProps.map(([from, to, color], i) => (
           <line
@@ -121,44 +105,44 @@ export default function Spiral({
             y1={from[1]}
             x2={to[0]}
             y2={to[1]}
-            stroke={color || stroke || "gray"}
+            stroke={color || stroke || 'gray'}
             strokeWidth={strokeWidth}
-            strokeLinecap={strokeLinecap || "round"}
+            strokeLinecap={strokeLinecap || 'round'}
           />
         ))}
         {(labels || []).map(({ angle, label, color, fill }, i) => {
-          const [x, y] = spiralPosition(angle, rad(angle), spin, ...center)
+          const [x, y] = spiralPosition(angle, rad(angle), spin, ...center);
           return (
             <g
               key={i}
-              style={{ cursor: "pointer" }}
+              style={{ cursor: 'pointer' }}
               onClick={() => onTrigger && onTrigger(i)}
             >
               <circle
                 cx={x}
                 cy={y}
                 r={fontSize}
-                fill={fill || stroke || "gray"}
+                fill={fill || stroke || 'gray'}
               />
               <text
                 x={x}
                 y={y + fontSize / 3}
                 textAnchor="middle"
-                fill={color || "white"}
+                fill={color || 'white'}
                 style={{
                   fontSize,
-                  pointerEvents: "none",
-                  userSelect: "none",
+                  pointerEvents: 'none',
+                  userSelect: 'none'
                 }}
               >
                 {label}
               </text>
             </g>
-          )
+          );
         })}
       </svg>
     </>
-  )
+  );
 }
 
 export function spiralPosition(
@@ -170,6 +154,6 @@ export function spiralPosition(
 ): [number, number] {
   return [
     Math.sin((spin - angle + 0.5) * Math.PI * 2) * radius + cx,
-    Math.cos((spin - angle + 0.5) * Math.PI * 2) * radius + cy,
-  ]
+    Math.cos((spin - angle + 0.5) * Math.PI * 2) * radius + cy
+  ];
 }
--- content/components/tuning/HarmonicSpiral.tsx
+++ content/components/tuning/HarmonicSpiral.tsx
@@ -1,27 +1,38 @@
-import React from "react"
-import Spiral, { SpiralProps, Line } from "../common/Spiral"
-import { angle, frequencyColor } from "../tuning/tuning"
+import React from 'react';
+import Spiral, { SpiralProps, Line } from '../common/Spiral';
+import { angle, frequencyColor } from '../tuning/tuning';
 
 export default function HarmonicSpiral(props: SpiralProps) {
-  const { min, max, zoom, spin, precision, strokeWidth } = props
+  const { min, max, onTrigger, hideLabels, hideLines } = props;
   function getLines(): Line[] {
-    return Array.from({ length: Math.pow(2, max - 1 - min) }, (_, i) => [
-      angle(i + 1) + min,
-      angle(i + 1) + min + 1,
-      frequencyColor((i + 1) * 440),
-    ])
+    if (hideLines) {
+      return [];
+    }
+    return Array.from(
+      { length: Math.pow(2, max - 1) - Math.pow(2, min) + 1 },
+      (_, i) => i + Math.pow(2, min)
+    ).map((n) => [angle(n), angle(n) + 1, frequencyColor(n * 440)]);
   }
   function getLabels() {
-    return (
-      Array.from({ length: Math.pow(2, max - min) }, (_, i) => i + 1)
-        /*.filter((n) => n === 2 || n % 2)*/
-        .map((n) => ({
-          label: n + "",
-          angle: angle(n) + min,
-          fill: frequencyColor(n * 440),
-          color: "black",
-        }))
-    )
+    if (hideLabels) {
+      return [];
+    }
+    return Array.from(
+      { length: Math.pow(2, max) - Math.pow(2, min) + 1 },
+      (_, i) => i + Math.pow(2, min)
+    ).map((n) => ({
+      label: n + '',
+      angle: angle(n),
+      fill: frequencyColor(n * 440),
+      color: 'black'
+    }));
   }
-  return <Spiral {...props} lines={getLines()} labels={getLabels()} />
+  return (
+    <Spiral
+      {...props}
+      lines={getLines()}
+      labels={getLabels()}
+      onTrigger={(i) => onTrigger && onTrigger(i + Math.pow(2, min))}
+    />
+  );
 }
