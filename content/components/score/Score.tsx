81dc236f9359088057dfed6230b9792047a571c6
Felix Roos <roos@entrecode.de>
2020-05-20 13:32:28 +0200

    pull out score render logic

--- content/components/score/Score.tsx
+++ content/components/score/Score.tsx
@@ -1,92 +1,28 @@
 // copy of https://github.com/markacola/react-vexflow/blob/master/src/index.js
 import React, { useRef, useEffect } from 'react';
 import Vex from 'vexflow';
-import { Note } from '@tonaljs/tonal';
+import { renderScore, ScoreProps } from './score';
 
 const VF = Vex.Flow;
-const { Formatter, Renderer, Stave, StaveNote } = VF;
+const { Renderer } = VF;
 
-const clefWidth = 30;
-const timeWidth = 30;
-
-export function Score({
-  staves = [],
-  clef = 'treble',
-  timeSignature = '4/4',
-  width = 450,
-  height = 150
-}) {
+export function Score(props: ScoreProps) {
+  props = {
+    staves: [],
+    clef: 'treble',
+    timeSignature: '4/4',
+    width: 450,
+    height: 150,
+    ...props
+  };
   const container = useRef();
   const rendererRef = useRef<any>();
-
   useEffect(() => {
-    //if (rendererRef.current == null) {
-    if (!rendererRef.current) {
-      rendererRef.current = new Renderer(
-        container.current,
-        Renderer.Backends.SVG
-      );
-    }
-    const renderer = rendererRef.current;
-    renderer.resize(width, height);
-    const context = renderer.getContext();
-    context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed');
-    const clefAndTimeWidth =
-      (clef ? clefWidth : 0) + (timeSignature ? timeWidth : 0);
-    const staveWidth = (width - clefAndTimeWidth) / staves.length;
-
-    let currX = 0;
-    staves.forEach((notes, i) => {
-      const stave = new Stave(currX, 0, staveWidth);
-      if (i === 0) {
-        stave.setWidth(staveWidth + clefAndTimeWidth);
-        clef && stave.addClef(clef);
-        timeSignature && stave.addTimeSignature(timeSignature);
-      }
-      currX += stave.getWidth();
-      stave.setContext(context).draw();
-
-      const processedNotes = notes
-        .map((note) => (typeof note === 'string' ? { key: note } : note))
-        .map((note) =>
-          Array.isArray(note) ? { key: note[0], duration: note[1] } : note
-        )
-        .map(({ key, ...rest }) =>
-          typeof key === 'string'
-            ? {
-                key: key.includes('/')
-                  ? key
-                  : `${Note.get(key).pc}/${Note.get(key).oct}`,
-                ...rest
-              }
-            : rest
-        )
-        .map(({ key, keys, duration = 'q' }) => {
-          keys = key ? [key] : keys;
-          const note = new StaveNote({
-            keys,
-            duration: String(duration)
-          });
-          keys.forEach((key, index) => {
-            const accidentals = Note.accidentals(key.replace('/', ''));
-            if (accidentals) {
-              note.addAccidental(index, new VF.Accidental(accidentals));
-            }
-          });
-          return note;
-        });
-
-      /* const beams = VF.Beam.generateBeams(processedNotes, {
-        groups: [new Vex.Flow.Fraction(1, 4)]
-      }); */
-      Formatter.FormatAndDraw(context, stave, processedNotes, {
-        auto_beam: true,
-        align_rests: false
-      });
-      /* beams.forEach((b) => b.setContext(context).draw()); */
-      /* stave.setContext(context).draw(); */
-    });
-  }, [staves]);
+    rendererRef.current =
+      rendererRef.current ||
+      new Renderer(container.current, Renderer.Backends.SVG);
+    renderScore({ renderer: rendererRef.current, ...props });
+  }, [props.staves]);
 
   return <div ref={container} />;
 }
---
+++ content/components/score/score.ts
@@ -0,0 +1,106 @@
+import Vex from 'vexflow';
+import { Note } from '@tonaljs/tonal';
+
+const VF = Vex.Flow;
+const { Formatter, Renderer, Stave, StaveNote } = VF;
+
+export declare interface ScoreProps {
+  staves?: any[];
+  clef?: 'treble' | 'bass';
+  timeSignature?: string;
+  width?: number;
+  height?: number;
+  renderer?: any;
+  container?: any;
+}
+
+export function renderScore({
+  staves = [],
+  clef = 'treble',
+  timeSignature = '4/4',
+  width = 450,
+  height = 150,
+  renderer,
+  container
+}: ScoreProps) {
+  const clefWidth = 30;
+  const timeWidth = 30;
+  renderer = renderer || new Renderer(container, Renderer.Backends.SVG)
+  renderer.resize(width, height);
+  const context = renderer.getContext();
+  context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed');
+  const clefAndTimeWidth =
+    (clef ? clefWidth : 0) + (timeSignature ? timeWidth : 0);
+  const staveWidth = (width - clefAndTimeWidth) / staves.length;
+
+  let currX = 0;
+  let allNotes = [];
+  let allProcessedNotes = [];
+  staves.forEach((notes, i) => {
+    const stave = new Stave(currX, 0, staveWidth);
+    if (i === 0) {
+      stave.setWidth(staveWidth + clefAndTimeWidth);
+      clef && stave.addClef(clef);
+      timeSignature && stave.addTimeSignature(timeSignature);
+    }
+    currX += stave.getWidth();
+    stave.setContext(context).draw();
+    allNotes = allNotes.concat(notes);
+    const processedNotes = notes
+      .map((note) => (typeof note === 'string' ? { key: note } : note))
+      .map((note) =>
+        Array.isArray(note) ? { key: note[0], duration: note[1] } : note
+      )
+      .map(({ key, ...rest }) =>
+        typeof key === 'string'
+          ? {
+            key: key.includes('/')
+              ? key
+              : `${Note.get(key).pc}/${Note.get(key).oct}`,
+            ...rest
+          }
+          : rest
+      )
+      .map(({ key, keys, duration = 'q' }) => {
+        keys = key ? [key] : keys;
+        const note = new StaveNote({
+          keys,
+          duration: String(duration)
+        });
+        if (note['dots']) {
+          note.addDotToAll();
+        }
+        keys.forEach((key, index) => {
+          const accidentals = Note.accidentals(key.replace('/', ''));
+          if (accidentals) {
+            note.addAccidental(index, new VF.Accidental(accidentals));
+          }
+        });
+        return note;
+      });
+    allProcessedNotes = allProcessedNotes.concat(processedNotes);
+    const beams = VF.Beam.generateBeams(processedNotes, {
+      // groups: [new Vex.Flow.Fraction(1, 4), new Vex.Flow.Fraction(1, 2)]
+      // this does not really work proficiently => wrong grouping
+    });
+    Formatter.FormatAndDraw(context, stave, processedNotes);
+    beams.forEach(function (b) {
+      b.setContext(context).draw();
+    });
+    allNotes.concat(processedNotes);
+  });
+
+  var ties = allProcessedNotes.map((note, index) =>
+    !index || !allNotes[index].tie
+      ? null
+      : new VF.StaveTie({
+        first_note: allProcessedNotes[index - 1],
+        last_note: allProcessedNotes[index],
+        first_indices: [0],
+        last_indices: [0]
+      })
+  );
+  ties.forEach(function (t) {
+    t && t.setContext(context).draw();
+  });
+}
