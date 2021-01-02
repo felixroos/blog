2bffac6f19c47bfebf1d1deb525a51a36b2445f2
Felix Roos <roos@entrecode.de>
2021-01-02 22:14:43 +0100

    rename chromaAverage to chromaCenter + fix some things in scale color post

--- content/components/sets/chromaCenter.ts
+++ content/components/sets/chromaCenter.ts
@@ -1,4 +1,4 @@
-export default function chromaAverage(chroma: string) {
+export default function chromaCenter(chroma: string) {
   const digits = chroma.split('');
   const ones = digits.map((d, i) => d === '1' ? i : -1).filter(i => i !== -1);
   const avg = ones.reduce((sum, i) => i + sum, 0) % 12;
--- content/components/sets/chromaColor.ts
+++ content/components/sets/chromaColor.ts
@@ -1,6 +1,6 @@
 import { interpolateSinebow } from 'd3-scale-chromatic';
-import chromaAverage from './chromaAverage';
+import chromaCenter from './chromaCenter';
 
 export default function chromaColor(chroma) {
-  return interpolateSinebow(chromaAverage(chroma) / 12);
+  return interpolateSinebow(chromaCenter(chroma) / 12);
 }
