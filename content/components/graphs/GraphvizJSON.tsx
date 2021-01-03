3c7e298d42869c3c7779843d196c0b3ea69c52ab
Felix Roos <roos@entrecode.de>
2021-01-03 22:25:45 +0100

    implementation detail boxes

--- content/components/graphs/GraphvizJSON.tsx
+++ content/components/graphs/GraphvizJSON.tsx
@@ -1,11 +1,11 @@
 import { Graphviz } from 'graphviz-react';
 import toDot from 'jgf-dot';
 
-export default ({ json, options }) => {
+export default function GraphvizJSON({ json, options }) {
   if (!json) {
     return;
   }
   const dot = toDot(json);
   // console.log('dot',dot);
   return <Graphviz dot={dot} options={options} />;
-};
+}
