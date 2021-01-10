866ac834fd41b5d8dce806f3165329866e0179f8
Felix Roos <roos@entrecode.de>
2021-01-10 13:59:10 +0100

    fix pruning

--- content/components/graphs/astar.test.ts
+++ content/components/graphs/astar.test.ts
@@ -1,10 +1,16 @@
+import chordScales from '../sets/chordScales';
+import scaleChroma from '../sets/scaleChroma';
 import scaleDifference from '../sets/scaleDifference';
+import scaleModes from '../sets/scaleModes';
 import astar, { Target } from './astar';
 
 test('astar', () => {
 
   const graph = [['D aeolian', 'D dorian'], ['G mixolydian'], ['C lydian', 'C major']];
   const getNodeID = (level, scale) => `${level}.${scale}`;
+  const colorDiff = (source, target) => {
+    return source && scaleChroma(source) !== scaleChroma(target) ? 1 : 0;
+  };
   const scaleTargets = (graph) => (nodeID) => {
     const [lvl, source] = nodeID.split('.');
     const level = parseInt(lvl);
@@ -13,12 +19,12 @@ test('astar', () => {
     }
     return graph[level + 1].map((target): Target => [
       getNodeID(level + 1, target),
-      scaleDifference(source, target)
+      scaleDifference(source, target) + colorDiff(source, target)
     ])
   }
   expect(getNodeID(0, 'D dorian')).toBe('0.D dorian');
 
-  expect(scaleTargets(graph)('1.G mixolydian')).toEqual([['2.C lydian', 2], ['2.C major', 0]]);
+  expect(scaleTargets(graph)('1.G mixolydian')).toEqual([['2.C lydian', 3], ['2.C major', 0]]);
 
   const start = graph[0].map(scale => getNodeID(0, scale));
   const lastLvl = graph.length - 1;
@@ -35,5 +41,52 @@ test('astar', () => {
   expect(scalePath(graph)).toEqual(['D dorian', 'G mixolydian', 'C major']);
   expect(scalePath([['D major']])).toEqual(['D major']);
   expect(scalePath([['D major'], ['D major', 'Db major']])).toEqual(['D major', 'D major']);
+
+  const attyaChords = ['Fm7', 'Bbm7', 'Eb7', 'Ab^7', 'Db^7', 'Dm7', 'G7', 'C^7', 'C^7', 'Cm7', 'Fm7', 'Bb7', 'Eb^7', 'Ab^7', 'Am7', 'D7', 'G^7', 'G^7', 'Am7', 'D7', 'G^7', 'G^7', 'F#h7', 'B7b9', 'E^7', 'C7b13', 'Fm7', 'Bbm7', 'Eb7', 'Ab^7', 'Db^7', 'Db-^7', 'Cm7', 'Bo7', 'Bbm7', 'Eb7', 'Ab^7', 'Gh7', 'C7b9'];
+  const attyaScales = attyaChords.map(chord => chordScales(chord, scaleModes('major', 'harmonic minor', 'melodic minor'), true));
+
+  expect(scalePath(attyaScales)).toEqual(
+    [
+      "F aeolian",
+      "Bb dorian",
+      "Eb mixolydian",
+      "Ab major",
+      "Db lydian",
+      "D dorian",
+      "G mixolydian",
+      "C major",
+      "C major",
+      "C aeolian",
+      "F dorian",
+      "Bb mixolydian",
+      "Eb major",
+      "Ab lydian",
+      "A dorian",
+      "D mixolydian",
+      "G major",
+      "G major",
+      "A dorian",
+      "D mixolydian",
+      "G major",
+      "G major",
+      "F# locrian",
+      "B phrygian dominant",
+      "E lydian",
+      "C altered",
+      "F aeolian",
+      "Bb dorian",
+      "Eb mixolydian",
+      "Ab major",
+      "Db lydian",
+      "Db- lydian",
+      "C phrygian",
+      "B ultralocrian",
+      "Bb dorian",
+      "Eb mixolydian",
+      "Ab major",
+      "G locrian",
+      "C phrygian dominant",
+    ]
+  );
 
 })
--- content/components/graphs/astar.ts
+++ content/components/graphs/astar.ts
@@ -1,6 +1,6 @@
 import { minIndex } from 'd3-array';
 declare type NodeID = string;
-declare type Connection = [value: number, source: NodeID | null, target: NodeID];
+declare type Connection = [source: NodeID | null, target: NodeID, value: number, h?: number];
 export declare type Target = [target: NodeID, value: number, h?: number];
 
 // TODO: use h distance
@@ -8,38 +8,58 @@ export declare type Target = [target: NodeID, value: number, h?: number];
 export default function astar(
   startNodes: NodeID[],
   endNodes: NodeID[],
-  getConnections: (node: NodeID) => Target[]
+  getTargets: (source: NodeID) => Target[]
 ): any[] {
-  let open: Connection[] = startNodes.map(node => [0, null, node]);
+  let open: Connection[] = startNodes.map(node => [null, node, 0]);
   const closed: Connection[] = [];
   let winner;
+  let count = 0;
   while (!winner) {
-    const bestIndex = minIndex(open, ([value]) => value);
+    count++;
+    const bestIndex = minIndex(open, (o) => o[2]);
     const best = open[bestIndex];
     closed.push(best);
-    winner = closed.find((c) => endNodes.includes(c[2]));
+    winner = closed.find((c) => endNodes.includes(c[1]));
     if (winner) {
+      /* console.log('count', count); */
       return traceWinner();
     }
-    const [distance, _, newSource] = best;
-    const connections: Connection[] = getConnections(newSource)
-      .map(([target, value]) => [distance + value, newSource, target]);
+    const [_, newSource, distance] = best;
+    const connections: Connection[] = getTargets(newSource)
+      .map(([target, value, h]): Connection => [newSource, target, distance + value, h])
     open = [
       ...open.slice(0, bestIndex),
       ...connections,
       ...open.slice(bestIndex + 1)
-    ]
+    ].filter(c => !closed.find(([_, target, v]) => target === c[1] && c[2] >= v))
   }
   function traceWinner() {
-    const path = winner.slice(1);
+    const path = winner.slice(0, 2);
     if (!path[0]) {
       return [path[1]];
     }
     while (!startNodes.includes(path[0])) {
-      const prev = closed.find((c) => c[2] === path[0]);
-      path.unshift(prev[1]);
+      const prev = closed.find((c) => c[1] === path[0]);
+      path.unshift(prev[0]);
     }
     return path;
   }
 }
 
+
+
+/*
+// attempt to unify min + pruning into one loop:
+    /* let best, bestIndex;
+    open = open.filter((c, i) => {
+      if (best !== undefined && c[2] >= best[2]) {
+        return true; // not min
+      }
+      // const obsolete = closed.find(([_, target, v]) => target === c[1] && c[2] >= v);
+      // if (obsolete) {
+      //   return false;
+      // }
+      best = c;
+      bestIndex = i;
+      return true;
+    }) */
