d38608a2611d574f30cc4f2d0f1752da7aba2dd0
Felix Roos <roos@entrecode.de>
2021-04-07 10:40:10 +0200

    slicker graphviz component

--- content/components/graphs/Graphviz.tsx
+++ content/components/graphs/Graphviz.tsx
@@ -1,56 +1,34 @@
-// taken from https://github.com/DomParfitt/graphviz-react/blob/master/src/Graphviz.tsx
-// adjusted to allow using attributer
-// https://www.npmjs.com/package/d3-graphviz
-
 import * as React from 'react';
-import { graphviz, GraphvizOptions } from 'd3-graphviz';
+import toDot from 'jgf-dot';
+import canUseDOM from '../../components/canUseDOM';
 
-export class Graphviz extends React.Component<IGraphvizProps, any> {
-  private static count = 0;
+canUseDOM() && document.write('<script src="https://cdn.jsdelivr.net/npm/@hpcc-js/wasm/dist/index.min.js"><' + '/script>');
 
-  private static defaultOptions: GraphvizOptions = {
-    fit: true,
-    height: 500,
-    width: 500,
-    zoom: false,
+export function Graph({ json, editable }: any) {
+  const [el, setEl] = React.useState<HTMLDivElement>();
+  const [value, setValue] = React.useState<string>(JSON.stringify(json, null, 2));
+  const renderGraph = async (jsonString) => {
+    if (!window?.['@hpcc-js/wasm'] || !el) {
+      return;
+    }
+    try {
+      const { graphviz } = window['@hpcc-js/wasm'];
+      const dot = toDot(JSON.parse(jsonString));
+      const svg = await graphviz.layout(dot, 'svg', 'dot');
+      el.innerHTML = svg;
+    } catch (error) {
+      console.log('invalid..');
+    }
   };
-
-  private id: string;
-
-  constructor(props: IGraphvizProps) {
-    super(props);
-    this.id = `graphviz${Graphviz.count}`;
-    Graphviz.count += 1;
-  }
-
-  public render = (): JSX.Element => {
-    const { className } = this.props;
-    return (
-      <div className={className} id={this.id} />
-    );
-  };
-
-  public componentDidMount = () => {
-    this.renderGraph();
-  };
-
-  public componentDidUpdate = () => {
-    this.renderGraph();
-  };
-
-  private renderGraph = () => {
-    const { dot, options } = this.props;
-    graphviz(`#${this.id}`)
-      .options({
-        ...Graphviz.defaultOptions,
-        ...options || {},
-      })
-      .renderDot(dot);
-  };
-}
-
-export interface IGraphvizProps extends React.ClassAttributes<Graphviz> {
-  dot: string;
-  options?: GraphvizOptions;
-  className?: string;
+  React.useEffect(() => {
+    renderGraph(value);
+  }, [window['@hpcc-js/wasm'], el, value]);
+  return (
+    <>
+      <div ref={(e) => setEl(e)}></div>
+      {editable && (
+        <textarea style={{ width: '100%', height: '600px' }} value={value} onChange={(e) => setValue(e.target.value)} />
+      )}
+    </>
+  );
 }
